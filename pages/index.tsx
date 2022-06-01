e1122e33c058c6b62d15d0f614939efe3150d702
keybig <100385397+keybig@users.noreply.github.com>
2022-06-01 04:31:51 +0000

    notify on change props check

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,7 +15,13 @@ import { useRouter } from 'next/router'
 
 const Home = () => {
 
-  const queryClient = new QueryClient()
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        notifyOnChangeProps: 'tracked',
+      },
+    },
+  })
 
 
   const router = useRouter()
