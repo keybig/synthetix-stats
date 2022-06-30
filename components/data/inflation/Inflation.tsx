c75968b54d2573324ca5b2a28e109c988b0a662a
keybig <100385397+keybig@users.noreply.github.com>
2022-06-30 10:35:43 -0400

    inflation line chart width

--- components/data/inflation/Inflation.tsx
+++ components/data/inflation/Inflation.tsx
@@ -50,7 +50,7 @@ const Inflation = ({
       </div>
 
      
-        <ResponsiveContainer width={"80%"} height={200}>
+        <ResponsiveContainer width={"100%"} height={200}>
           <LineChart data={click === 1 ? inflationDataMain : click === 10 ? inflationDataOvm : inflationDataAll}>
             <Line
               type="linear"
