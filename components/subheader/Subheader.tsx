37e7ce448004166e6a23cf504809ab74dffbb861
keybig <100385397+keybig@users.noreply.github.com>
2022-06-30 00:56:39 -0400

    remove subheading description

--- components/subheader/Subheader.tsx
+++ components/subheader/Subheader.tsx
@@ -5,9 +5,6 @@ const Subheader = () => {
   return (
     <div className={styles.container}>
         <h1 className={styles.heading}>Synthetix Stats</h1>
-        <p className={styles.description}>
-        The Synthetix HERO Stats page. Some of the most important stats for the community.
-       </p>
     </div>
   )
 }
