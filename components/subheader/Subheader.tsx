9cf246fc11c3688b0d31adf437e3894e6c4d804c
keybig <100385397+keybig@users.noreply.github.com>
2022-07-06 19:36:44 -0400

    subheader text

--- components/subheader/Subheader.tsx
+++ components/subheader/Subheader.tsx
@@ -5,6 +5,7 @@ const Subheader = () => {
   return (
     <div className={styles.container}>
         <h1 className={styles.heading}>Synthetix Stats</h1>
+        <p className={styles.description}>The Synthetix HERO Stats page, with some of the most important stats for the community</p>
     </div>
   )
 }
