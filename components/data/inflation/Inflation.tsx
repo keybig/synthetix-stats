1f2fa0f2d050653d49153a0b5360ac9bd224988c
keybig <100385397+keybig@users.noreply.github.com>
2022-07-06 00:14:09 -0400

    start inflation box resize and some css

--- components/data/inflation/Inflation.tsx
+++ components/data/inflation/Inflation.tsx
@@ -11,7 +11,7 @@ import {
 } from "recharts";
 import { formatNumber } from "../../../constants/format";
 import CustomToolTip from './inflationTT'
-import {RiInformationFill} from "react-icons/ri"
+import { RiInformationFill } from "react-icons/ri"
 import InfoTooltip from '../../infoToolTip/InfoTooltip'
 
 
@@ -35,7 +35,7 @@ const Inflation = ({
   inflationDataMain,
   inflationDataOvm,
   inflationDataAll
-}:Inflation) => {
+}: Inflation) => {
 
   const allReward = currentRewardMain + currentRewardOvm
   const allTime = allTimeInflationMain + allTimeInflationOvm
@@ -48,15 +48,15 @@ const Inflation = ({
     <div className={styles.wrapper}>
       <div className={styles.keyWrap}>
         <div className={styles.titleRow}>
-        <h3 className={styles.inflationTitle}>Inflation Fees (SNX Rewards)</h3>
-        <InfoTooltip content={ttInfo}>
+          <h3 className={styles.inflationTitle}>Inflation Fees (SNX Rewards)</h3>
+          <InfoTooltip content={ttInfo}>
 
-      <span 
-        className={styles.icon}
-        >
-      <RiInformationFill/>
-      </span>
-      </InfoTooltip>
+            <span
+              className={styles.icon}
+            >
+              <RiInformationFill />
+            </span>
+          </InfoTooltip>
         </div>
         <h5 className={styles.subtitle}>Current Epoch</h5>
         <p className={styles.currentEpoch}>{click === 1 ? formatNumber.format(currentRewardMain) : click === 10 ? formatNumber.format(currentRewardOvm) : formatNumber.format(allReward)}</p>
@@ -64,22 +64,22 @@ const Inflation = ({
         <p className={styles.toDate}>{click === 1 ? formatNumber.format(allTimeInflationMain) : click === 10 ? formatNumber.format(allTimeInflationOvm) : formatNumber.format(allTime)}</p>
       </div>
 
-     
-        <ResponsiveContainer width={"100%"} height={200}>
-          <LineChart data={click === 1 ? inflationDataMain : click === 10 ? inflationDataOvm : inflationDataAll}>
-            <Line
-              type="linear"
-              dataKey="snx_rewards"
-              stroke="#8884d8"
-              strokeWidth={3}
-            />
-            <Tooltip content={<CustomToolTip/>}/>
-            <YAxis domain={["dataMin - 200000", "dataMax + 15000"]} hide={true}/>
-            <XAxis dataKey={"date"} fontSize={14} interval={"preserveStartEnd"} />
-          </LineChart>
-        </ResponsiveContainer>
-     
-      
+
+      <ResponsiveContainer width={"80%"} height={200}>
+        <LineChart data={click === 1 ? inflationDataMain : click === 10 ? inflationDataOvm : inflationDataAll}>
+          <Line
+            type="linear"
+            dataKey="snx_rewards"
+            stroke="#8884d8"
+            strokeWidth={3}
+          />
+          <Tooltip content={<CustomToolTip />} />
+          <YAxis domain={["dataMin - 200000", "dataMax + 15000"]} hide={true} />
+          <XAxis dataKey={"date"} fontSize={14} interval={"preserveStartEnd"} />
+        </LineChart>
+      </ResponsiveContainer>
+
+
     </div>
   );
 };
