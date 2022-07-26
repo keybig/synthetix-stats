9b91f716646b0a48bd79cbc38e8922d56d414d75
keybig <100385397+keybig@users.noreply.github.com>
2022-07-25 22:30:56 -0400

    add favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,6 +11,8 @@ function MyApp({ Component, pageProps,  }: AppProps) {
     <Head>
       <title>SNX Stats</title>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      <link rel="icon" type="image/png" href="/favicon/favicon-32x32.png" sizes="32x32" />
+			<link rel="icon" type="image/png" href="/favicon/favicon-16x16.png" sizes="16x16" />
     </Head>
     
     <Layout>
