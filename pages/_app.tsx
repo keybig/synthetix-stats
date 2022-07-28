8dcc82edb3cb7784128ff48a89879b8e1ef4df2a
keybig <100385397+keybig@users.noreply.github.com>
2022-07-28 10:57:59 -0400

    update favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,8 +11,7 @@ function MyApp({ Component, pageProps,  }: AppProps) {
     <Head>
       <title>SNX Stats</title>
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      <link rel="icon" type="image/png" href="/favicon/favicon-32x32.png" sizes="32x32" />
-			<link rel="icon" type="image/png" href="/favicon/favicon-16x16.png" sizes="16x16" />
+      <link rel="shortcut icon" href="/favicon/favicon.ico"/>
     </Head>
     
     <Layout>
